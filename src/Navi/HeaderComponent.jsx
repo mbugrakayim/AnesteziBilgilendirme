016d0b59340e5818ec23a5996275de4330d2a0f3
mbugrakayim <mbugrakayim@gmail.com>
2025-03-20 06:33:06 +0300

    Navi margin güncellendi

--- src/Navi/HeaderComponent.jsx
+++ src/Navi/HeaderComponent.jsx
@@ -7,13 +7,13 @@ function HeaderComponent() {
       <header>
         <nav className="navbar navbar-expand-md mt-4">
           <div className="container-fluid justify-content-center">
-            <Link to="/ameliyatöncesi" className="btn btn-success mx-2 btn-lg">
+            <Link to="/ameliyatöncesi" className="btn btn-success mx-2 mb-2 btn-lg">
               Ameliyat öncesi
             </Link>
-            <Link to="/ameliyatgünü" className="btn btn-success mx-2 btn-lg">
+            <Link to="/ameliyatgünü" className="btn btn-success mx-2 mb-2 btn-lg">
               Ameliyat günü
             </Link>
-            <Link to="/ameliyatsonrası" className="btn btn-success mx-2 btn-lg">
+            <Link to="/ameliyatsonrası" className="btn btn-success mx-2 mb-2 btn-lg">
               Ameliyat sonrası
             </Link>
           </div>
